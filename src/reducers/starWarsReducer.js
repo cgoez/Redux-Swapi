25530e8f79468276b25c96cd8c146e91cca23789
cgoez <goez2001@gmail.com>
2018-04-18 14:35:24 -0700

    Create swapi reducer cases

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,37 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING, FETCHED, ERROR } from "../actions";
+
+// define a few properties here.
+// Array chars, Boolean fetching, Boolean fetched, null error.
 const initialState = {
-  // define a few properties here.
-  // Array chars, Boolean fetching, Boolean fetched, null error.
+  chars: [],
+  fetching: false,
+  fetched: false,
+  error: ""
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      // If only fetching trigger fetching boolean
+      return Object.assign({}, state, { fetching: true });
+    case FETCHED:
+      return Object.assign({}, state, {
+        // If request succeeds build out chars array
+        // Set fetched boolean to true
+        // Also set fetching boolean to false since no longer fetching
+        chars: [...state.chars, ...action.payload],
+        fetched: true,
+        fetching: false
+      });
+    case ERROR:
+      return Object.assign({}, state, {
+        // Not fetching, set fetching boolean to false
+        fetching: false,
+        error: "Error fetching Swapi, please try again"
+      });
     default:
       return state;
   }
