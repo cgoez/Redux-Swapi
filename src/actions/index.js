6ca2bfe521171bdfc2120b8dcde25037c29125c4
cgoez <goez2001@gmail.com>
2018-04-18 14:48:59 -0700

    Create swapi actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for fetched and one for errors
+
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const ERROR = "ERROR";
 
 // our action creator will be a function that returns a promise
 // we'll have to be sure to make our promise resolve within our new "thunk based middlware"
@@ -9,8 +13,19 @@ import axios from 'axios';
 // remember that now we have controll over our thunk-based
 
 export const fetchSwapi = () => {
-    const request = axios.get('https://swapi.co/api/people/');
-    return (dispatch) => {
-        request.then
-    }
-}
+  const promise = axios.get("https://swapi.co/api/people/");
+  return dispatch => {
+    // dispatch FETCHING (first state)
+    dispatch({ type: FETCHING });
+    promise
+      .then(response => {
+        // 2nd state dispatch if success, response.data.RESULTS (check api)
+        dispatch({ type: FETCHED, payload: response.data.results });
+      })
+      .catch(error => {
+        console.log(error);
+        // 2nd state dispatch if fail -> error
+        dispatch({ type: ERROR });
+      });
+  };
+};
